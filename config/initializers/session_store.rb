b5bf5eede1ccfa1cbe274f8fe09121d2d58fd24b
Peter Cooper <git@peterc.org>
2008-12-08 03:36:56 +0000

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+    ActionController::Base.session = { :session_key => '_VGSCDQ_session', :secret => 'LBFKHSLVEQBZLQYIXBSFOYOLBHOZDUHFCULWCXHB' }
+    ActionController::Base.session_store = :active_record_store
